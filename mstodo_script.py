4d28d6c4cc3ec0cff59e4f46646888643a489a53
codingwatermelon <32379178+codingwatermelon@users.noreply.github.com>
2023-08-25 14:22:42 -0700

    Update mstodo_script.py

--- mstodo_script.py
+++ mstodo_script.py
@@ -1,18 +1,38 @@
+#
+# Title:        mstodo_script.py
+# Author:       @codingwatermelon
+# Date:         8/25/23
+#
+# Function:     Transform data from MS Graph API
+#
+# Description:  Transform data from MS Graph API to send to
+#               my LED matrix
+#
+# TODO:
+#   - Add "sad path" measures to this code
+#
+# Notes:
+#   - All API requests are staggered with a 2 second wait
+#
+
+
 import json
 from os.path import exists
 import requests
 import datetime
 import time
 
+# Function to unload config file into variable and return it
 def read_config(config_file):
     if exists(config_file):
         with open(config_file, "r") as f:
             config = json.load(f)
     else:
-        print("[ERROR] Couldn't find file '" + config_file + "'")
+        print("[ERROR] Couldn't find file '" + config_file + "'. Ensure file exists and use 'config.json.sample' to as a template for the variables that need to be set up.")
 
     return config
 
+# Function to load json config into output file for debugging purposes
 def output_json_to_file(json_obj, output_file):
     try:
         with open(output_file, "w") as out:
@@ -21,8 +41,8 @@ def output_json_to_file(json_obj, output_file):
     except:
         print("[ERROR] Could not write given json to file '" + output_file + "'")
 
-
-# Function to generate access token using refresh token
+# Function to generate access token using refresh token and returns the current access token
+# This is used when the access token on file is expired
 def get_access_token(config, config_file):
     scopes = ['Tasks.Read']
 
@@ -39,12 +59,14 @@ def get_access_token(config, config_file):
         'client_secret': config["client_secret"]
     }
 
-    # Note: Use 'data' for POST-style body info and 'params' for GET-style URL params
-    # Source: https://stackoverflow.com/questions/15900338/python-request-post-with-param-data
     print("[DEBUG] Making POST request to refresh access token...")
+    
+    # Note: Use 'data' param for POST-style body info and 'params' param for GET-style URL params
+    # Source: https://stackoverflow.com/questions/15900338/python-request-post-with-param-data
     response = requests.post(refresh_token_endpoint, headers=refresh_token_headers, data=refresh_token_params).json()
     time.sleep(2)
 
+    # Replace access token key on file with current access token
     # Source: https://stackoverflow.com/questions/21035762/python-read-json-file-and-modify
     with open(config_file, "r+") as f:
         data = json.load(f)
@@ -57,16 +79,16 @@ def get_access_token(config, config_file):
 
     return response["access_token"]
 
-config_file = "T:\code\matrix\MSToDo-API-Scripts\config.json"
-
+# Set variables
+base_path = "T:\code\matrix\MSToDo-API-Scripts"
+config_file = base_path + "\config.json"
 config = read_config(config_file)
+access_token = config["curr_access_token"]
 
 authority_url = 'https://login.microsoftonline.com/consumers/'
-
 graph_url = 'https://graph.microsoft.com/v1.0/'
 
 complete = False
-access_token = config["curr_access_token"]
 
 while not complete:
     # Use auth token to run API requests
@@ -81,7 +103,6 @@ while not complete:
 
     print("[DEBUG] Making GET request for info...")
     response = requests.get(endpoint, headers=headers).json()
-
     time.sleep(2)
 
     if 'error' in response:
@@ -89,7 +110,7 @@ while not complete:
         access_token = get_access_token(config, config_file)
         config = read_config(config_file)
     else:
-        output_json_to_file(response, "T:\\code\\matrix\\MSToDo-API-Scripts\\Archive\\" + datetime.datetime.now().strftime("%m%d%y-%H%M%S") + "_test.json")
+        output_json_to_file(response, base_path + "\\Archive\\" + datetime.datetime.now().strftime("%m%d%y-%H%M%S") + "_test.json")
 
         tasks = {}
 
@@ -100,7 +121,7 @@ while not complete:
                 # TODO Order tasks by due date 
                 # if "dueDateTime" in task and datetime.datetime.strptime(task["dueDateTime"]["dateTime"].split("T")[0], "%Y-%m-%d").date() == datetime.datetime.now().date():
             
-                # Deal with duplicate task names
+                # Handle duplicate task names
                 taskName = task["title"]
                 i = 1
 
